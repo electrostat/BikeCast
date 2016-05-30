25c03407007072476a4df57f86c9ba0012833f09
electrostat <agbyaj@gmail.com>
2016-05-29 20:47:26 -0400

    0.1
    - forecast.io integration
    - will call and get weather forecast based on location

---
+++ BikeCast/app/src/main/java/agbytech/com/bikecast/Forecast.java
@@ -0,0 +1,74 @@
+package agbytech.com.bikecast;
+
+import android.location.Location;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by Anthony-Parkour on 5/29/16.
+ */
+public class Forecast {
+    private final String LOG_TAG="BikeCast";
+
+    public void getForecast(final Location location){
+
+        //track most recent outdoor location point for reporting
+        new Thread(){
+            public void run(){
+                HttpURLConnection connection = null;
+
+                try{
+                    URL myURL = new URL("https://api.forecast.io/forecast/" + API_KEY + "/" + location.getLatitude() + "," + location.getLongitude());
+                    connection = (HttpsURLConnection)myURL.openConnection();
+                    connection.setRequestMethod("GET");
+
+                    connection.connect();
+
+                    InputStream iStream = connection.getInputStream();
+
+                    String response = readStream(iStream);
+
+                    Log.e(LOG_TAG, "Forecast Response: " + response);
+
+                }
+                catch (MalformedURLException ex){
+                    Log.e(LOG_TAG, "Invalid URL", ex);
+                }
+                catch (IOException ex){
+                    Log.e(LOG_TAG, "IO/Connection Error", ex);
+                }finally {
+                    if(connection == null){
+                        connection.disconnect();
+                    }
+
+                    currentThread().interrupt();
+                }
+//
+            }
+        }.start();
+    }
+
+
+    private String readStream(InputStream is) {
+
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+}
--- BikeCast/app/src/main/java/agbytech/com/bikecast/MainActivity.java
+++ BikeCast/app/src/main/java/agbytech/com/bikecast/MainActivity.java
@@ -1,15 +1,26 @@
 package agbytech.com.bikecast;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //initial location zoom
+    private LocationManager gpsLocManager;
+    private LocationManager netLocManager;
+
+    private static final int REQUEST_CODE_PERMISSION = 2;
+
+    public Forecast forecast = new Forecast();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +33,52 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                getCurrentLocation();
             }
         });
+
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+//        if (ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                    REQUEST_CODE_PERMISSION);
+//        }
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,
+//                                           String permissions[], int[] grantResults) {
+//        switch (requestCode) {
+//            case REQUEST_CODE_PERMISSION: {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//                    // permission was granted, yay! Do the
+//                    // contacts-related task you need to do.
+//
+//                    getCurrentLocation();
+//
+//                } else {
+//
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                }
+//                return;
+//            }
+//
+//            // other 'case' lines to check for other
+//            // permissions this app might request
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +101,48 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void getCurrentLocation() {
+        //set criteria
+        Criteria criteria = new Criteria();
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+
+        gpsLocManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+        gpsLocManager.getBestProvider(criteria, true);
+        netLocManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+        netLocManager.getBestProvider(criteria, true);
+
+//        if (ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                    REQUEST_CODE_PERMISSION);
+//        }
+        gpsLocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, burstListener);
+        netLocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 0, burstListener);
+
+
+    }
+
+    private LocationListener burstListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+            gpsLocManager.removeUpdates(burstListener);
+            netLocManager.removeUpdates(burstListener);
+
+            forecast.getForecast(location);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
+
+
 }
