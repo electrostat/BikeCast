c37c8ade46091823adc1620a2d2a2b12444b1daf
electrostat <agbyaj@gmail.com>
2017-03-29 08:04:05 -0400

    Initialize boolean class
    - start process of building weather booleans
    - change fab icon

---
+++ BikeCast/app/src/main/java/agbytech/com/bikecast/WeatherBool.java
@@ -0,0 +1,18 @@
+package agbytech.com.bikecast;
+
+/**
+ * Created by Anthony-Parkour on 3/29/17.
+ */
+
+public class WeatherBool {
+//    double precipIntensity
+//    double precipProbability
+//    double windSpd
+//    double feelTemp
+//    double humidity
+//    double temperature
+//    double dewpoint
+//    double visibility
+//    double pressure
+//    double ozone
+}
