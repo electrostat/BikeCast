c6db10be6203054060aace7b24564df721f656a6
electrostat <agbyaj@gmail.com>
2017-04-08 11:16:15 -0400

    Preference Page Design
    - add button
    - table layout
    - name changes

--- BikeCast/app/src/main/java/agbytech/com/bikecast/PreferenceActivity.java
+++ BikeCast/app/src/main/java/agbytech/com/bikecast/PreferenceActivity.java
@@ -1,13 +1,18 @@
 package agbytech.com.bikecast;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by Anthony-Parkour on 6/16/16.
  */
 public class PreferenceActivity extends AppCompatActivity {
+
+    private final String LOG_TAG="BikeCast";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,15 @@ public class PreferenceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_preference);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addBool);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(LOG_TAG, "add clicked" +
+                        "");
+            }
+        });
 
     }
 }
