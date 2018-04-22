45a97aea17352afa38745fcf324f450969e5ebde
electrostat <agbyaj@gmail.com>
2018-04-22 00:54:55 -0400

    Clean-up code
    - clean up code before pushing

--- BikeCast/app/src/main/java/agbytech/com/bikecast/MainActivity.java
+++ BikeCast/app/src/main/java/agbytech/com/bikecast/MainActivity.java
@@ -183,33 +183,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode,
-//                                           String permissions[], int[] grantResults) {
-//        switch (requestCode) {
-//            case REQUEST_CODE_PERMISSION: {
-//                // If request is cancelled, the result arrays are empty.
-//                if (grantResults.length > 0
-//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//
-//                    // permission was granted, yay! Do the
-//                    // contacts-related task you need to do.
-//
-//                    getCurrentLocation();
-//
-//                } else {
-//
-//                    // permission denied, boo! Disable the
-//                    // functionality that depends on this permission.
-//                }
-//                return;
-//            }
-//
-//            // other 'case' lines to check for other
-//            // permissions this app might request
-//        }
-//    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -462,7 +435,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @SuppressLint("ResourceAsColor")
     private void updateNowLight(boolean status) {
         if (status) {
             nowLight.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.colorGo)));
@@ -471,7 +443,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @SuppressLint("ResourceAsColor")
     private void updateAmLight(boolean status) {
         if (status) {
             amLight.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.colorGo)));
@@ -480,7 +451,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @SuppressLint("ResourceAsColor")
     private void updatePmLight(boolean status) {
         if (status) {
             pmLight.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.colorGo)));
